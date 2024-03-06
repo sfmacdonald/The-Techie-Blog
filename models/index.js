b93f236e8f30975d1402e2b273bd1a8e5ffbed82
sfmacdonald <seanfmacdonald@icloud.com>
2024-03-05 21:12:31 -0700

    add js files for models

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comments');
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
